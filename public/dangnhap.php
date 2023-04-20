524e26e08fbe98c9d1bd14ec162b7da024c318fd
Martel-359 <doanhuuvinh0108@gmail.com>
2023-04-20 14:49:38 +0700

    hoan thanh

---
+++ public/dangnhap.php
@@ -0,0 +1,57 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) { // neu trang thai chua duoc bat 
+    session_start(); //if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+}
+require_once '../bootstrap.php';
+use CT275\Labs\admin;
+use CT275\Labs\khach_hang;
+$errors = [];
+$message = "Đăng nhập thất bại - Vui lòng đăng nhập lại";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $khach_hang_db = new khach_hang($PDO);
+    $khach_hang_formdbs = $khach_hang_db->all();
+    $khach_hang_2 = new khach_hang($PDO);
+    $khach_hang_dangnhap = $khach_hang_2->fill($_POST);
+    foreach ($khach_hang_formdbs as $khach_hang_formdb) :
+        if (($khach_hang_formdb->email == $khach_hang_dangnhap->email) && ($khach_hang_formdb->mat_khau) ==  $khach_hang_dangnhap->mat_khau) {
+            $_SESSION['khach_hang_formdb'] = 'me';
+            $_SESSION['role'] = 2;
+            $_SESSION['id'] = $khach_hang_formdb->id;// id_kh
+            $_SESSION['email'] = $khach_hang_formdb->email;
+            $_SESSION['mat_khau'] = $khach_hang_formdb->mat_khau;
+            $_SESSION['ten'] = $khach_hang_formdb->ten;
+            //$_SESSION['vaitro']= $khach_hang_formdb->vaitro;
+           // $khachhang->save_id_kh($khach_hang_formdb->getId());
+           // echo ('ban da dang nhap th
+            redirect('hanghoa.php');
+           }   
+    endforeach;
+    
+    
+
+    $admin_db = new admin($PDO);
+    $admin_formdbs = $admin_db->all();
+    $admin_2 = new admin($PDO);
+    $admin_dangnhap = $admin_2->fill($_POST);
+    foreach ($admin_formdbs as $admin_formdb) :
+        if (($admin_formdb->email == $admin_dangnhap->email) && $admin_formdb->mat_khau == $admin_dangnhap->mat_khau) {
+        $_SESSION['admin_formdb'] = 'admin';
+        $_SESSION['role'] = 1;
+        $_SESSION['email'] = $admin_formdb->email;
+        $_SESSION['mat_khau'] = $admin_formdb->mat_khau;
+       $_SESSION['ten'] = $admin_formdb->ten;
+        redirect('admin.php');
+       }    
+    endforeach;
+ 
+    echo "<script type='text/javascript'>alert('$message');</script>";
+     echo ('
+  <div style="style="padding-bottom: 100px; margin-top:100px; width: 50%; padding-top: 100px; position: relative" class="text-center">
+  <h1 style ="text-align: center; margin-top: 100px;"><p> Đăng nhập thất bại !</p></h1>
+    <div style ="text-align: center;">
+  <a href="index.php"> <button style ="background-color: #0d6efd; color: white; font-size: 14px;border: none; padding: 12px 26px;cursor: pointer;" class="btn btn-primary">Đi đến trang chủ</button></a>
+  <a href="hanghoa.php"> <button style ="background-color: #0d6efd; color: white; font-size: 14px;border: none; padding: 12px 26px;cursor: pointer;" class="btn btn-primary">Đi đến trang sản phẩm</button></a>
+  </div>
+  </div>');
+}
+
---
+++ public/uploads/data.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `loai_hang_hoa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ten_loai` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `loai_hang_hoa` (`ten_loai`) VALUES
+('Laptop','2023-04-11 20:54:03'),
+('SamSung','2023-04-11 20:54:03'),
+('iPhone');
+
+
+CREATE TABLE `hang_hoa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ten_hang_hoa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gia` int(11) NOT NULL,
+  `so_luong_hang` int(11) NOT NULL,
+  `hinh` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mo_ta` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_loai` int(11) NOT NULL,
+  `ngaynhap` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `fk_hang_hoa_loai_hang_hoa` FOREIGN KEY (`id_loai`) REFERENCES `loai_hang_hoa` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+INSERT INTO `hang_hoa` ( `ten_hang_hoa`,`gia`, `so_luong_hang`, `hinh`, `mo_ta`, `id_loai`,`ngaynhap`) VALUES
+('Điện thoại Samsung Galaxy Note 10+',15900000, 90, '12333.png', 'mo_ta', 2,'2023-04-11 20:54:03'),
+('Điện thoại Samsung Galaxy A21s (6GB/64GB)',52900000, 96, 'samsung-galaxy-a21s-055620-045627-400x460.png', 'mo_ta', 2,'2023-04-11 20:51:03'),
+('Điện thoại Samsung Galaxy Note 20 Ultra',29900000, 97, 'samsung-galaxy-note-20-ultra-vangdong-400x460-400x460.png', 'mo_ta', 2,'2023-04-11 21:54:03'),
+('Điện thoại Samsung Galaxy Z Flip',36000000, 96, 'samsung-galaxy-z-flip-chitiet-2-788x544.png', 'mo_ta', 2,'2023-04-11 20:14:03'),
+('Điện thoại Samsung Galaxy Z Fold2 5G',50000000, 97, 'samsung-galaxy-z-fold-2-123620-093621-400x460.png', 'mo_ta', 2,'2023-04-11 20:12:03'),
+('Điện thoại iPhone 8 Plus 128GB',14900000, 99, 'ip8s.jpg', 'mo_ta', 3,'2023-04-11 20:41:03'),
+('Điện thoại iPhone 7 Plus 128GB', 11490000, 99, 'iphone-7-plus-128gb-de-400x460.png', 'mo_ta', 3,'2023-04-11 20:14:13'),
+('Điện thoại iPhone SE 64GB (2020)',12900000, 49, 'iphone-se-2020-do-400x460-400x460.png', 'mo_ta', 3,'2023-04-11 20:24:23'),
+('Điện thoại iPhone 11 Pro 256GB',31900000, 26, 'ip11pr.jpg', 'mo_ta', 3,'2023-04-11 20:34:33'),
+('Laptop HP Envy 13 ba0046TU i5 1035G4/8GB/512GB/Office H&S2019/Win10 (171M7PA)',22900000, 19, 'hp-envy-13-ba0046tu-i5-171m7pa-225859-600x600.jpg', 'mo_ta', 1,'2023-04-21 20:44:33'),
+('Laptop Asus VivoBook Gaming F571GT i7 9750H/8GB/512GB/120Hz/4GB GTX1650/Win10 (AL858T)',24490000, 18, 'asus-vivobook-gaming-f571gt-i7-al858t-226256-600x600.jpg', 'mo_ta', 1,'2023-04-11 20:04:43'),
+('Laptop Lenovo IdeaPad Slim 3 15IIL05 i3 1005G1/4GB/512GB/Win10 (81WE003RVN)', 25000000, 5, 'lenovo-ideapad-3-15iil05-i3-81we003rvn-013920-053901-600x600.jpg', 'mo_ta',1,'2023-04-11 20:01:11')
+CREATE TABLE `khach_hang` (
+  `id` bigint(20) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+  `ten` varchar(255) COLLATE utf8mb4_unicode_ci ,
+  `dia_chi` varchar(255) COLLATE utf8mb4_unicode_ci ,
+  `so_dien_thoai` varchar(255) COLLATE utf8mb4_unicode_ci ,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE ,
+  `mat_khau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- pass=123456(sha1)
+INSERT INTO `khach_hang` (`ten`, `dia_chi`, `so_dien_thoai`, `email`,`mat_khau`) VALUES
+('doanhuuvinh','angiang','0912345687','doanhuuvinh@gmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b','2023-04-11 20:54:03'),
+('ducvinh','cantho','0984568267','ducvinh@gmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b');
+
+CREATE TABLE `admin` (
+  `id` bigint(20) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  `ten` varchar(255) COLLATE utf8mb4_unicode_ci ,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE ,
+  `mat_khau` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--pass=123456(sha1)
+INSERT INTO `admin` (`ten`,`email`,`mat_khau`) VALUES
+('doanhuuvinh','vinhadmin@gmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b','2023-04-11 20:54:03'),
+('ducvinh','ducvinhadmin@gmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b');
